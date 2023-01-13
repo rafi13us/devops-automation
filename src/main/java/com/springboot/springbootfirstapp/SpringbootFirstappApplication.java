829cae79afdaa81be426b583e784c104ffaeab63
rafi13us <100405574+rafi13us@users.noreply.github.com>
2023-01-13 22:49:47 +0000

    intialcommit

---
+++ src/main/java/com/springboot/springbootfirstapp/SpringbootFirstappApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.springbootfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootFirstappApplication {
+
+	@GetMapping
+	public String message(){
+		return "Congratulations, successfully deployed using Kubernetes";
+	}
+		public static void main(String[] args) {
+		SpringApplication.run(SpringbootFirstappApplication.class, args);
+	}
+
+}
