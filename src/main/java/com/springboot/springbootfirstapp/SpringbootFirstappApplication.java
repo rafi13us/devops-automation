52d7fdd865ce8e55f4791003ba9ab8f2c481d477
rafi13us <100405574+rafi13us@users.noreply.github.com>
2023-01-14 00:37:42 +0000

    second commit

--- src/main/java/com/springboot/springbootfirstapp/SpringbootFirstappApplication.java
+++ src/main/java/com/springboot/springbootfirstapp/SpringbootFirstappApplication.java
@@ -11,7 +11,7 @@ public class SpringbootFirstappApplication {
 
 	@GetMapping
 	public String message(){
-		return "Congratulations, successfully deployed using Kubernetes";
+		return "Congratulations, successfully deployed using Kubernetes k*s";
 	}
 		public static void main(String[] args) {
 		SpringApplication.run(SpringbootFirstappApplication.class, args);
